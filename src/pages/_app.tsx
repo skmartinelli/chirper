3794837f58c189c38962c23e42b6cd635f8c1c53
skm010 <skm010@ucsd.edu>
2023-10-03 09:38:36 -0700

    add auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,17 @@ import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
 
+import { ClerkProvider } from "@clerk/nextjs";
+import type { AppProps } from "next/app";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return(
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+
+  )
 };
 
 export default api.withTRPC(MyApp);
