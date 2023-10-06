ff725b135521a764816c6ab7cfb5217128fd8e87
skm010 <skm010@ucsd.edu>
2023-10-05 17:49:04 -0700

    change img to Image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ dayjs.extend(relativeTime);
       
 
 import { api } from "~/utils/api";
+import Image from "next/image";
 
 const CreatePostWizard = () => {
   const {user} = useUser();
@@ -21,11 +22,11 @@ const CreatePostWizard = () => {
   if (!user) return null;
 
   return <div className="flex w-full gap-4 ">
-    <img src= {user.imageUrl} 
+    <Image src= {user.imageUrl} 
     alt="Profile image" 
     className=" h-20 w-20 rounded-full"/>
     <input placeholder="Type something fun!" className="bg-transparent grow outline-none"></input>
-  </div>
+  </div>  
 
 }
 type PostWithUser = RouterOutputs["posts"]["getAll"][number]
@@ -34,7 +35,7 @@ const PostView = (props: PostWithUser) => {
   const {post, author } = props;
   return (
   <div key={post.id} className="p-4 gap-3 border-b border-slate-400 flex">
-    <img src={author.profilePicture}  className=" h-14 w-14 rounded-full" />
+    <Image src={author.profilePicture}  className=" h-14 w-14 rounded-full" alt={`@${author.username}'s profile picture`} />
 
     <div className="flex flex-col ">
       <div className="flex gap-2 font-bold text-slate-300"><span>{`@${author.username} `}</span>
